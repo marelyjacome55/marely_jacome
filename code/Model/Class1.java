4c097bf6889130d33dcc79db2b998a185dbe3bc5
Marely <marelyjacome782@gmail.com>
2025-09-26 10:15:31 -0600

    Agrego carpeta Model a la rama code

---
+++ code/Model/Class1.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Class1 {
+
+    /**
+     * Default constructor
+     */
+    public Class1() {
+    }
+
+    /**
+     * 
+     */
+    public void Port1;
+
+}
